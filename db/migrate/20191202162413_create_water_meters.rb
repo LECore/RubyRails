3ce1db7c7ef4d1ec45eb830ef786da5b56c15c95
Leonid Epishev <lecore2011@gmail.com>
2019-12-04 00:40:03 +0300

    fix migration

---
+++ db/migrate/20191202162413_create_water_meters.rb
@@ -0,0 +1,9 @@
+class CreateWaterMeters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :water_meters do |t|
+      t.date :check_date
+      t.float :hot
+      t.float :cold
+    end
+  end
+end
